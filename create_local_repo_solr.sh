b3c95015fdcf9c5885420ded43453c79ead4c440
Ubuntu <ubuntu@instance-2.c.impalacloud111.internal>
2017-09-21 21:45:39 +0000

    Finalize ambari-solr6. Add Banana UI to quicklinks

--- create_local_repo_solr.sh
+++ create_local_repo_solr.sh
@@ -3,12 +3,12 @@
 # Author: Sudharsan R
 # Purpose: Create a local repo for SOLR
 
-SOLR_VERSION=6.6.1
+export SOLR_VERSION=6.6.1
 tempFolder=/tmp
 appName=HDP-SOLR
 appId=HDP-SOLR-2.6-100
 basefolder=/opt/lucidworks-hdpsearch
-
+packageName=solr
 
 
 rootfolder=$tempFolder/$appName
@@ -25,6 +25,7 @@ wget http://www.namesdir.com/mirrors/apache/lucene/solr/$SOLR_VERSION/solr-$SOLR
 tar zxvf solr-$SOLR_VERSION.tgz
 git clone https://github.com/lucidworks/banana.git
 mv banana solr-$SOLR_VERSION/server/solr-webapp/webapp/.
+mv solr-$SOLR_VERSION solr
 rm solr-$SOLR_VERSION.tgz
 
 # Create DEBIAN control file
@@ -45,17 +46,22 @@ cd $tempFolder
 dpkg-scanpackages $appName /dev/null | gzip -9c > $rootfolder/Packages.gz
 
 # Install package in webserver
-sudo mkdir -p /var/www/html/$appId/repos/ubuntu14/$appName && cd /var/www/html/$appId/repos/ubuntu14/$appName
+#sudo mkdir -p /var/www/html/$appId/repos/ubuntu14/$appName && cd /var/www/html/$appId/repos/ubuntu14/$appName
+sudo mkdir -p /var/www/html/$appId/repos/ubuntu14/dists/$appName/Release/binary-amd64 && cd /var/www/html/$appId/repos/ubuntu14/dists/$appName/Release/binary-amd64
 sudo mv $rootfolder/solr.deb $rootfolder/Packages.gz ./
 
+sudo ln -sf /var/www/html/$appId/repos/ubuntu14/dists/$appName/Release /var/www/html/$appId/repos/ubuntu14/dists/$appName/main
+
+# Commented. These will be written by Ambari.
 # Write repo information
-cd /etc/apt/sources.list.d && sudo bash -c "cat <<EOF > solr.list
-#deb http://$(hostname -f)/solr/ubuntu14 solr/
-deb http://$(hostname -f)/$appId/repos/ubuntu14 $appName/
-EOF"
+#cd /etc/apt/sources.list.d && sudo bash -c "cat <<EOF > solr.list
+#deb http://$(hostname -f)/$appId/repos/ubuntu14 $appName/
+#EOF"
 
 # Install local repo
-sudo apt-get update && sudo apt-get install solr
+#sudo apt-get update && sudo apt-get install solr
 
-# Remove temporary files
+# Remove temporary and downloaded files
 rm -Rf $rootfolder
+
+
